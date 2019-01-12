e692be2bfd85b48526f641e39da22b8f46a374a3
Max Amzarakov <6706341+maxam18@users.noreply.github.com>
2019-01-12 22:38:38 +0300

    formatting

--- tests/log_test.c
+++ tests/log_test.c
@@ -1,7 +1,7 @@
-/* C My Lib log test 
+/* C My Lib log test
  * File: log_test.c
  * Started: Fri Feb  2 20:58:04 MSK 2018
- * Author: Max Amzarakov (maxam18 _@_ gmail _._ com) 
+ * Author: Max Amzarakov (maxam18 _@_ gmail _._ com)
  * Copyright (c) 2018 ...
  */
 
@@ -11,13 +11,13 @@ int main(void)
 {
     char filename[] = "/tmp/test.log";
     char progname[] = "test_prog";
-    
+
     cml_log_init(CML_LOG_FLAG_PRIO | CML_LOG_FLAG_TIME, filename, progname);
 
     cml_log(CML_LOG_NOTICE, "test notification");
     cml_log(CML_LOG_NOTICE, "test reopen");
     cml_log_reopen(filename);
     cml_log(CML_LOG_INFO, "Log reopened");
-    
+
     return 0;
 }
